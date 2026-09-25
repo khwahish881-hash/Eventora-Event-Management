b3408dbd01cf5d2f7eb3ce9708c89f935d92682f
Khwahish <creativelabcanva61@gmail.com>
2026-09-25 22:05:29 +0530

    Fix event model import for Render

--- server/routes/event.js
+++ server/routes/event.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const Event = require("../models/Event");
+const Event = require("../models/event");
 
 const {
     protect,
