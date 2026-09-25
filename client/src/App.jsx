fdfd9094e2784d5a516f7d16793a6caee06b1cf5
Khwahish <creativelabcanva61@gmail.com>
2026-09-25 23:39:45 +0530

    Fix Login import case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import {
   useLocation
 } from "react-router-dom";
 
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VerifyOtp from "./pages/VerifyOtp";
 import ForgotPassword from "./pages/ForgotPassword";
